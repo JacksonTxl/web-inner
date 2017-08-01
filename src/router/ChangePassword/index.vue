bcbefd92f1f62261ed8384c984627e0e091eadff
JacksonTxl <172004133@qq.com>
2017-08-01 18:30:10 +0800

    add changepassword verify

--- src/router/ChangePassword/index.vue
+++ src/router/ChangePassword/index.vue
@@ -1,8 +1,8 @@
 <template>
   <main class="change-password">
-      <s-input placeholder="输入当前登录密码" type="password" :isCode="false" :maxlength="16" :params="pre_password"></s-input>
-      <s-input placeholder="密码由6-16个字符组成，区分大小写" type="password" :isCode="false" :maxlength="16" :params="new_password"></s-input>
-      <s-input placeholder="再次输入新密码" type="password" :isCode="false" :maxlength="16" :params="confirm_password"></s-input>
+      <s-input placeholder="输入当前登录密码" type="password" :isCode="false" :maxlength="16" :params="pre_password" @keyup="checkPsw(false,'pre_password')" v-model="pre_password.input.value" @focus="checkFocus('pre_password')" @blur="checkBlur('pre_password')"></s-input>
+      <s-input placeholder="密码由6-16个字符组成，区分大小写" type="password" :isCode="false" :maxlength="16" :params="new_password" @keyup="checkPsw(false,'new_password')" v-model="new_password.input.value" @focus="checkFocus('new_password')" @blur="checkBlur('new_password')"></s-input>
+      <s-input placeholder="再次输入新密码" type="password" :isCode="false" :maxlength="16" :params="confirm_password" @keyup="checkPsw(false,'confirm_password')" v-model="confirm_password.input.value" @focus="checkFocus('confirm_password')" @blur="checkBlur('confirm_password')"></s-input>
       <s-button :disabled="button.disabled" :label="button.label"></s-button>
   </main>
 </template>
@@ -10,11 +10,14 @@
 <script>
   import Input from '../../components/Input.vue';
   import Button from '../../components/Button.vue';
+  import CONSTANT from '../../util/constant';
 
   export default {
 
     data () {
       document.title = '修改密码';
+
+
       return {
         pre_password: {
           required: true,
@@ -27,7 +30,7 @@
             value: ''
           },
           tips: {
-            label: '您输入的信息有误，不能为空,长度6~16',
+            label: '',
             class: '',
             show: true
           },
@@ -51,7 +54,7 @@
             value: ''
           },
           tips: {
-            label: '密码6~16位字符组成，区分大小写，不能和旧密码一样',
+            label: '',
             class: '',
             show: true
           },
@@ -75,7 +78,7 @@
             value: ''
           },
           tips: {
-            label: '再次输入新密码 ',
+            label: '',
             class: '',
             show: true
           },
@@ -101,7 +104,62 @@
       SButton: Button
     },
     methods: {
+      checkFocus (param) {
+        if (param === 'pre_password') {
+          this.pre_password.input.class = 'focus';
+        } else if (param === 'new_password') {
+          this.new_password.input.class = 'focus';
+        } else if (param === 'confirm_password') {
+          this.confirm_password.input.class = 'focus';
+        }
+      },
+      checkBlur (type) {
+        this.checkPsw(true, type);
+      },
+      checkPsw (flag, type) {
+        const TIP1 = '您输入的信息有误，不能为空,长度6~16';
+        const TIP2 = '密码6~16位字符组成，区分大小写，不能和旧密码一样';
+        const TIP3 = '再次输入新密码';
+        var prePsw = CONSTANT.methods.checkPsw(this.pre_password.input.value);
+        var newPsw = CONSTANT.methods.checkPsw(this.new_password.input.value);
+        var conPsw = (this.confirm_password.input.value === this.new_password.input.value) && this.confirm_password.input.value.length >= 6;
 
+        if (prePsw && newPsw && conPsw) {
+          this.button.disabled = {};
+        } else {
+          this.button.disabled = {disabled: 'disabled'};
+        }
+        if (prePsw && type === 'pre_password') {
+          this.pre_password.input.class = 'success';
+          this.pre_password.tips.label = '';
+        } else if (!prePsw && type === 'pre_password' && ((this.pre_password.input.value.length >= 6 && this.pre_password.input.value.length <= 16) || flag)) {
+          this.pre_password.input.class = 'error';
+          this.pre_password.tips.label = TIP1;
+        } else if (type === 'pre_password') {
+          this.pre_password.input.class = 'focus';
+          this.pre_password.tips.label = '';
+        }
+        if (newPsw && type === 'new_password') {
+          this.new_password.input.class = 'success';
+          this.new_password.tips.label = '';
+        } else if (!newPsw && type === 'new_password' && ((this.new_password.input.value.length >= 6 && this.new_password.input.value.length <= 16) || flag || this.new_password.input.value === this.pre_password.input.value)) {
+          this.new_password.input.class = 'error';
+          this.new_password.tips.label = TIP2;
+        } else if (type === 'new_password') {
+          this.new_password.input.class = 'focus';
+          this.new_password.tips.label = '';
+        }
+        if (conPsw && type === 'confirm_password') {
+          this.confirm_password.input.class = 'success';
+          this.confirm_password.tips.label = '';
+        } else if (!conPsw && type === 'confirm_password') {
+          this.confirm_password.input.class = 'error';
+          this.confirm_password.tips.label = TIP3;
+        } else if (type === 'confirm_password') {
+          this.confirm_password.input.class = 'focus';
+          this.confirm_password.tips.label = '';
+        }
+      }
     }
   };
 </script>
--- src/util/constant.js
+++ src/util/constant.js
@@ -1,19 +1,24 @@
 export default {
   basic: {},
   regexp: {
-    //校验手机号的正则
+    // 校验手机号的正则
     PHONE: /^1[34578]\d{9}$/
   },
   methods: {
-  //  校验手机号码返回结果
-    checkPhone(value){
-      if(!value&&value!=11)return;
+    //  校验手机号码返回结果
+    checkPhone (value) {
+      if (!value && value.length != 11) return;
       return /^1[34578]\d{9}$/.test(value);
     },
-  //  校验手机验证码返回结果
-    checkPhoneCode(value){
-      if(!value&&value!=4)return;
+    //  校验手机验证码返回结果
+    checkPhoneCode (value) {
+      if (!value && value.length != 4) return;
       return /^\d{4}$/.test(value);
+    },
+    //  检验输入密码6~16位，不包含空格
+    checkPsw (value) {
+      if (!value && (value.length >= 6 || value.length <= 16)) return;
+      return /^(?:\d+|[a-zA-Z]+|[!@#$%^&*]+){6,16}$/.test(value);
     }
   }
 
